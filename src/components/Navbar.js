2291c73570723cf2a37977cfd98493eefbc1f5c5
Maxim <makarovmr@msk.bcs.ru>
2024-06-11 13:26:24 +0200

    Add release title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 import { vibrate } from '../telegram';
 
 const Navbar = () => {
@@ -7,22 +8,28 @@ const Navbar = () => {
         vibrate(200); // Вибрация на 200 миллисекунд при каждом нажатии на кнопку
     };
 
-    return (
-        <nav className="navbar">
-            <NavLink to="/" exact className="nav-item" activeClassName="active" onClick={handleButtonClick}>
-                Home
-            </NavLink>
-            <NavLink to="/notes" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
-                Notes
-            </NavLink>
-            <NavLink to="/add" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
-                Add Note
-            </NavLink>
-            <NavLink to="/edit" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
-                Edit Note
-            </NavLink>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-header">
+        <h1>Build Number: 1.1 </h1>
+      </div>
+      <NavLink to="/" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
+        Home
+      </NavLink>
+      <NavLink to="/notes" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
+        Notes
+      </NavLink>
+      <NavLink to="/add" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
+        Add Note
+      </NavLink>
+      <NavLink to="/edit" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
+        Edit Note
+      </NavLink>
+      <NavLink to="/profile" className="nav-item" activeClassName="active" onClick={handleButtonClick}>
+        Profile
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navbar;
