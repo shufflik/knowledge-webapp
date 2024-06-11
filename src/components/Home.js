bba7710d1e7929e31198a5b21c0e8ac8733f0eb5
Maxim <makarovmr@msk.bcs.ru>
2024-06-11 13:40:09 +0200

    Refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
       <div>
           <h1>Home</h1>
-          <h1>Build: 1.2 </h1>
+          <h1>Build: 1.2.1 </h1>
           <p>Welcome to the note-taking app!</p>
       </div>
   );
