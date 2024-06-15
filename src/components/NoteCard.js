8f87cb27db833cb63ade319f957bb403cadf6a7e
Maxim <makarovmr@msk.bcs.ru>
2024-06-15 18:30:12 +0200

    Fix errors

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './NoteCard.css';
 
